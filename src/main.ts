cbec5200edfd8cda0deaa6ce6c3475ce87a446fa
M4TH3US17 <matheusdalvino50@gmail.com>
2024-09-10 20:08:12 -0300

    docs/.env.example-SERVER_HOST

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const port = configService.get<number>('SERVER_PORT');
+  const host = configService.get<number>('SERVER_HOST'); 
   const environmentProfile = configService.get<number>('APPLICATION_DEV_ENVIRONMENT');
 
   app.useGlobalPipes(new ValidationPipe({
@@ -30,7 +31,7 @@ async function bootstrap() {
   await app.listen(port);
 
   console.log(`>>> Development environment? ${environmentProfile ? 'yes' : 'no'}`);
-  console.log(`>>> Application running at: http://localhost:${port}`);
+  console.log(`>>> Application running at: http://${host}:${port}`);
   console.log(`>>> Swagger URI: http://localhost:${port}/api`);
 }
 
